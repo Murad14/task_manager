c5a1a5dc0a2d7561d5a3b5f6f03548011ffb8851
Murad <49407545+Murad14@users.noreply.github.com>
2023-12-20 18:19:41 +0600

    modified

---
+++ backend/task_manager/tasks/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    
+    path('login/', TokenObtainPairView.as_view(), name='login_view'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
+    
+]
